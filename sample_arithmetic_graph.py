efe518807998fe62703e2f5471168a039bf15716
Kaixi Hou <kaixih@nvidia.com>
2022-05-12 10:02:14 -0700

    support arithmetic opt

---
+++ sample_arithmetic_graph.py
@@ -0,0 +1,20 @@
+import os
+# This arithmetic optimization on pow op only happens on CPUs.
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+import tensorflow as tf
+
+from tf_op_graph_utils import print_op_graph
+
+m, n, k = (3, 3, 4)  # Matrix dimensions
+precision = tf.float32
+
+def pow_model(x):
+  x = tf.cast(x, precision)
+
+  out = tf.math.pow(x, 3.0)
+  return out
+
+print_op_graph(pow_model, (m, k), "gelu_arithmetic_pass.png",
+               ['arithmetic'])
+
