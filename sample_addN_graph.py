c18d305950c3e9261eb5f77cae8057a09020e05f
Kaixi Hou <kaixih@nvidia.com>
2022-06-15 13:09:18 -0700

    support backprop

--- sample_addN_graph.py
+++ sample_addN_graph.py
@@ -16,20 +16,25 @@ def multi_add_model(x):
   w1 = _weight([r, s, c, k])
   w2 = _weight([r, s, c, k])
   w3 = _weight([r, s, c, k])
-  y0 = tf.nn.conv2d(x, w0, 1, 'SAME')
-  y1 = tf.nn.conv2d(x, w1, 1, 'SAME')
-  y2 = tf.nn.conv2d(x, w2, 1, 'SAME')
-  y3 = tf.nn.conv2d(x, w3, 1, 'SAME')
-
-  z0 = tf.math.add(y0, y1)
-  z1 = tf.math.add(y2, y3)
-  add_out = tf.math.add(z0, z1)
-
   wt = _weight([r, s, c, k])
-  out = tf.nn.conv2d(add_out, wt, 1, 'SAME')
-  out = tf.nn.relu(out)
-  return tf.identity(out)
 
-print_op_graph(multi_add_model, (n, h, w, c), "multi_add_to_one_add_n.png",
+  with tf.GradientTape() as g:
+    g.watch(x)
+    # Add a relu node to make sure the conv2d_backprop is not the last node.
+    x0 = tf.nn.relu(x)
+    y0 = tf.nn.conv2d(x0, w0, 1, 'SAME')
+    y1 = tf.nn.conv2d(y0, w1, 1, 'SAME')
+    y2 = tf.nn.conv2d(y0, w2, 1, 'SAME')
+    y3 = tf.nn.conv2d(y0, w3, 1, 'SAME')
+
+    add_out = tf.math.add(y1, y2)
+    add_out = tf.math.add(add_out, y3)
+    out = tf.nn.relu(add_out)
+    loss = tf.reduce_sum(out)
+  grads = g.gradient(loss, x)
+
+  return grads
+
+print_op_graph(multi_add_model, (n, h, w, c), "demo.png",
                ["arithmetic"])
 
